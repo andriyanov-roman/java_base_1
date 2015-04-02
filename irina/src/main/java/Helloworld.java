57f53a2f2cc906cf279213536083530b330759ed
irynamoiseeva <irynamoiseeva@gmail.com>
2015-04-02 20:39:54 +0300

    opeartions with variables

---
+++ irina/src/main/java/Helloworld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by irina on 4/2/2015.
+ */
+public class Helloworld {
+public  operations() {
+    byte firstargument;
+    byte secondargument;
+    byte multiple;
+    multiple = firstargument * secondargument;
+}
+}
