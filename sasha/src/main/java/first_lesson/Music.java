827d33b8ae8f188cb24f63da62d41c2eb7f00fc3
asphere <asemenov05@gmail.com>
2015-04-02 20:13:20 +0300

    nok-nok

---
+++ sasha/src/main/java/first_lesson/Music.java
@@ -0,0 +1,10 @@
+package first_lesson;
+
+/**
+ * Created by Андрей on 02.04.2015.
+ */
+public class Music {
+    public static void main(String[] args){
+        System.out.println("Nok-nok");
+    }
+}
