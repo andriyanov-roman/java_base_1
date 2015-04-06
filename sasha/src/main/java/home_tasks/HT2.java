c6b3efbb1214e678ad7482f766b78d7c80d47d6d
asphere <asemenov05@gmail.com>
2015-04-06 11:18:39 +0300

    home task 2

--- sasha/src/main/java/home_tasks/HT2.java
+++ sasha/src/main/java/home_tasks/HT2.java
@@ -4,7 +4,7 @@ public class HT2 {
 
     public static void main(String[] args) {
 
-        int x = 121, y = 20, z = 30, x1, x2, x3, x4, x5, x6, x7, x8;
+        int x = 10, y = 20, z = 30, x1, x2, x3, x4, x5, x6, x7, x8;
 
         if (x >= 30)
             System.out.println("The biggest variable is x = " + x);
@@ -15,23 +15,6 @@ public class HT2 {
         if (z >= 30)
             System.out.println("The biggest variable is z = " + z);
 
-        x1 = x%2;
-        x2 = x1%2;
-        x3 = x2%2;
-        x4 = x3%2;
-        x5 = x4%2;
-        x6 = x5%2;
-        x7 = x6%2;
-        x8 = x7%2;
-
-        System.out.println(x1);
-        System.out.println(x2);
-        System.out.println(x3);
-        System.out.println(x4);
-        System.out.println(x5);
-        System.out.println(x6);
-        System.out.println(x7);
-        System.out.println(x8);
 
         }
 
