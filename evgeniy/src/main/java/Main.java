df304ec5fc4212fd24ca0975a9dba82568bda5ac
vortor <krotov1212@mail.ru>
2015-04-06 22:11:33 +0300

    test

--- evgeniy/src/main/java/Main.java
+++ evgeniy/src/main/java/Main.java
@@ -1,15 +1,30 @@
 
 class Main {
     public static void main(String[] args) {
-        int a = 2; //byte зарезервированое задание для компелтора
-        //byte x = a; //short  двара раза больше чем байт
-        int b = 1;
+
+        byte a = 2+1; //byte зарезервированое задание для компелтора
+        //byte a2 = 1;
+        short c = 5-1;
+        //short c2 = 3;
+        int b = 6/2;
+        int b2= 2;
+        int b3= 1;
+        int b4= 1;
+        long d = 2*3;
 
         //System.out.println(x = a);
 
-        System.out.println(a = a);
-        System.out.println(b = a - b);
-        System.out.println(a = a - b);
+        System.out.println(a += a);
+        System.out.println(a = (byte) ((c+c)/a));
+        System.out.println(b = (c/c)*(a/a));
+
+        System.out.println(b4=b2);
+        System.out.println(b2=b3);
+        System.out.println(b3=b4);
+
+        System.out.println(b2=b2+b3);
+        System.out.println(b3=b2-b3);
+        System.out.println(b2=b2-b3);
     }
 
 }
