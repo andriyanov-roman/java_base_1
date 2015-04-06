76f9d7f2a9bd71c1ec8f658a0970ddac82204ba9
vortor <krotov1212@mail.ru>
2015-04-06 21:45:52 +0300

    test

---
+++ evgeniy/src/main/java/Home_task_1_1.java
@@ -0,0 +1,16 @@
+class Home_task_1_1 {
+    public static void hwork(String [] args){
+        int a=2;
+       /*
+        short b = 3-1;
+        int c = 8/2;
+        long d = 2*3;
+        */
+
+        //System.out.println(a =(byte) (1+2));
+        //System.out.println(a += a);
+        System.out.println(a = a+a);
+
+
+    }
+}
--- evgeniy/src/main/java/Main.java
+++ evgeniy/src/main/java/Main.java
@@ -1,6 +1,4 @@
-/**
- * Created by user on 4/2/2015.
- */
+
 class Main {
     public static void main(String[] args) {
         int a = 2; //byte зарезервированое задание для компелтора
@@ -9,11 +7,9 @@ class Main {
 
         //System.out.println(x = a);
 
-        System.out.println(a = a + b);
+        System.out.println(a = a);
         System.out.println(b = a - b);
         System.out.println(a = a - b);
-//
-        //System.out.println("Hello");
     }
 
 }
