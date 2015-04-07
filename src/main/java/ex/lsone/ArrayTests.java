d721d7665cf22b508ddedf2682b37e2e2e09b626
randriyanov <randriyanov@mail.ru>
2015-04-07 20:57:36 +0300

    added examples

--- src/main/java/ex/lsone/ArrayTests.java
+++ src/main/java/ex/lsone/ArrayTests.java
@@ -7,10 +7,19 @@ public class ArrayTests {
     public static void main(String[] args) {
         int i = 0;
         while (i <= 999) {
-            if(i > 50 || i < 100) {
+            if (i > 50 || i < 100) {
                 System.out.println(i);
             }
             i += 3;
         }
+        for (int j = 0; j <= 999; j += 3) {
+            if (j > 50 || j < 100) {
+                System.out.println(j);
+            }
+        }
+        int j = 0;
+        do {
+            System.out.println(j);
+        }while (j != 0);
     }
 }
