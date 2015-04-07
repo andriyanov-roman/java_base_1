44a2f293e6985ce5b6c294021f4add9182043dde
Eugene <deyneko55@gmail.com>
2015-04-07 16:43:59 +0300

    Largest variable

--- evgen/src/main/java/Hometask2_1.java
+++ evgen/src/main/java/Hometask2_1.java
@@ -1,11 +1,15 @@
 /**
  * Created by Евгений on 07.04.2015.
  */
-public class Hometask2_1 {
-        public static void main(String[] args) {
+    public class Hometask2_1
+    {
+        public static void main(String[] args)
+        {
+
             int x = 3;
             int y = -7;
             int z = 6;
+
             if (x > y && x > z) {
                 System.out.println("The larbyte i = a;gest naumber is X! Value =" + x);
             } else if (y > x && y > z) {
@@ -16,6 +20,6 @@ public class Hometask2_1 {
                 System.out.println("Error");
             }
         }
-}
+    }
 
 
