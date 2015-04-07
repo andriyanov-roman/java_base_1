23f16162ba3b081d50d6b1b74defacdee73b7144
randriyanov <randriyanov@mail.ru>
2015-04-07 20:47:41 +0300

    added examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,28 +3,24 @@
  */
 public class Main {
     public static void main(String[] args) {
-        byte firstVar = 2;
-        byte b = 2;
-        //System.out.println(a + b);
-        short c = 2;
-        short d = 2;
-        //System.out.println(c + d);
-        //int e = 2;
-        //long l = 10000;
-        float f = 5.67865F;
-        double z = 8.86899763;
-        char h = 'h';
-        char e = 'e';
-        char l = 'l';
-        char o = 'o';
-        System.out.println("" + h + e + l + o);
-        System.out.println(e);
-        System.out.println(l);
-        System.out.println(o);
-        boolean bool = false;
-        //a = (byte) c;
-        //+-/* ++ --
-//        System.out.println(a++);
-//        System.out.println(a);
+        int value = 120;
+        int temp;
+        int one, two, three, four, five, six, seven, eight;
+        one = value % 2;
+        temp = value / 2;
+        two = temp % 2;
+        temp = temp / 2;
+        three = temp % 2;
+        temp = temp / 2;
+        four = temp % 2;
+        temp = temp / 2;
+        five = temp % 2;
+        temp = temp / 2;
+        six = temp % 2;
+        temp = temp / 2;
+        seven = temp % 2;
+        temp = temp / 2;
+        eight = temp % 2;
+        System.out.println(eight + "" + seven + "" + six + "" + five + "" + four);
     }
 }
