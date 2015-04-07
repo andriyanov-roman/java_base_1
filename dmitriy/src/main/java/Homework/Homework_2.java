2e65b8d85e047fbb87c64dd70e973fcac9fe9bad
dirutix <dirutix@list.ru>
2015-04-07 19:04:32 +0300

    Homework 07.04

---
+++ dmitriy/src/main/java/Homework/Homework_2.java
@@ -0,0 +1,18 @@
+package Homework;
+
+/**
+ * Created by Дима on 07.04.2015.
+ */
+public class Homework_2 {
+    public static void main (String[] args){
+        int x, y, z;
+        x = 4;
+        y = 5;
+        z = 6;
+
+        if (x >= 6) System.out.println("The biggest Arg is x");
+        if (y >= 6) System.out.println("The biggest Arg is y");
+        if (z >= 6) System.out.println("The biggest Arg is z");
+
+    }
+}
