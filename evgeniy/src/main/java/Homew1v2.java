19213a3152e83806c6bf4793c7208334fcbe97ff
vortor <krotov1212@mail.ru>
2015-04-07 19:27:27 +0300

    test11

--- evgeniy/src/main/java/Homew1v2.java
+++ evgeniy/src/main/java/Homew1v2.java
@@ -4,11 +4,11 @@
 public class Homew1v2 {
     public static void main(String[] args){
         int b1 = 2, b2 = 3, b3 = 4 ;
-        if (b1>b2 || b1>b2) b1=b1;
+        if (b1>b2 && b1>b2) b1=b1;
         else
-        if (b2>b1 || b2>b3) b2=b2;
+        if (b2>b1 && b2>b3) b2=b2;
         else
-        if (b3>b1 || b3>b2) b3=b3;
+        if (b3>b1 && b3>b2) b3=b3;
 
     }
 }
