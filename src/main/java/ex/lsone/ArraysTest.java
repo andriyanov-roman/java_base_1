47253562c4a2c11e3cf6a7a0d6f73ccdb5e8d10b
randriyanov <randriyanov@mail.ru>
2015-04-09 20:19:52 +0300

    added examples

--- src/main/java/ex/lsone/ArraysTest.java
+++ src/main/java/ex/lsone/ArraysTest.java
@@ -5,21 +5,19 @@ package ex.lsone;
  */
 public class ArraysTest {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        long[] d = {a, b, c, (int) 8.9, 9};
-        boolean[] booleans = {true, false, true, false};
-        int[] aRRay = {1, 33, 2, 4, 4, 6, 6, 4, 3, 5, 6, 7, 8,};
-        System.out.println(aRRay[1]);
-        int[] array = new int[5];
-        array[0] = 5;
-        array[1] = 15;
-        array[2] = 51;
-        array[3] = 115;
-        array[4] = 151;
-        System.out.println(array[0] + array[1] + array[2] + array[3] + array[4]);
+        int[] array = {1, 33, 2, 4, 4, 6, 6, 4, 3, 5, 6, 7, 8,};
+        //Просто вывести массив
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+        System.out.println();
+        //Найти сумму массива
         int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = array[i] + sum;
+        }
+        System.out.println(sum);
+        //Вывести массив в обратном порядке
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i] + " ");
         }
