ec9f526b2475e27ec2305bec4751ced1f2305771
Dima <dirutix@list.ru>
2015-04-09 23:40:10 +0300

    Some changes

--- dmitriy/src/main/java/Homework/Homework_2.java
+++ dmitriy/src/main/java/Homework/Homework_2.java
@@ -4,7 +4,7 @@ package Homework;
  * Created by Дима on 07.04.2015.
  */
 public class Homework_2 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         int x, y, z;
         x = 4;
         y = 5;
@@ -13,6 +13,15 @@ public class Homework_2 {
         if (x >= 6) System.out.println("The biggest Arg is x");
         if (y >= 6) System.out.println("The biggest Arg is y");
         if (z >= 6) System.out.println("The biggest Arg is z");
+        //перевод числа из десятичной системы в двоичную
+        int value = 57;
+        int temp;
+        for (value = 57; value > 1; value /= 2) {
+            temp = value % 2;
+            System.out.println(temp + "");
+        }
+        System.out.println(1);
+
 
     }
 }
