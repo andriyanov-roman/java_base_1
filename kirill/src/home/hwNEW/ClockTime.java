6d94703497424cb3ed0d9af32583e491d2e67ed3
Fijji <Neponatno@bigmir.net>
2015-04-10 17:42:28 +0300

    home work 3

--- kirill/src/home/hwNEW/ClockTime.java
+++ kirill/src/home/hwNEW/ClockTime.java
@@ -5,20 +5,29 @@ public class ClockTime {
     public static void main(String[] args) {
         int x=0,y,zx,bx,zy,by,count=0;
 
-        while (x<=24){
-            y=0;
-            while(y<=60){
-                zx=x/100;
-                bx=x%100;
-                zy=y/100;
-                by=y%100;
-                if(zx==zy && bx==by){count++;};
-                if(zx==by && bx==zy){count++;};
-                y++;}
+        while (x!=24){
+            y=59;
+            while(y>=1){
+                if (x>9) zx=x/10;
+                else zx=0;
+                bx=x%10;
+                if(y>9)
+                    zy=y/10;
+                else zy=0;
+                by=y%10;
+
+                if(zx==zy){
+                    if (bx==by){
+                        count++;
+                        System.out.println(zx+""+bx+":"+zy+""+by);}}
+
+                else if (zx==by){
+                    if(bx==zy){
+                        count++;
+                        System.out.println(zx+""+bx+":"+zy+""+by);}}
+                y--;}
             x++;
         }
-
         System.out.println("Simmetrichnix pozici chasov i minut v sutki: "+count);
     }
-
 }
