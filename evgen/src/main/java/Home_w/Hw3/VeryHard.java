b3b4a3056f173ba984c3aac54107573b29bab872
Eugene <deyneko55@gmail.com>
2015-04-14 17:44:29 +0300

    Hard part almost done...

---
+++ evgen/src/main/java/Home_w/Hw3/VeryHard.java
@@ -0,0 +1,43 @@
+package Home_w.Hw3;
+
+/**
+ * Created by Евгений on 14.04.2015.
+ */
+//Написать программу, которая перемещает в конец массива все элементы, значения которых находится в отрезке [a,b]
+public class VeryHard {
+    public static void main(String[] args) {
+        {
+            int[] arr = {6, 8, 2, 7, 9, 1, 3, 4, 7, 0};
+            int a, b, z, i, k, p, r;
+
+            a = 6;
+            b = 8;
+            p = 9;
+
+            System.out.print("Заданный массив: ");
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+
+            for (r = 0; r < p; r++) {
+                for (i = 0; i < p; i++) {
+                    for (k = a; k <= b; k++) {
+                        if (arr[i] == k) {
+                            z = arr[p];
+                            arr[p] = arr[i];
+                            arr[i] = z;
+                            p--;
+                        }
+                    }
+                }
+            }
+
+
+            System.out.print("Новый массив: ");
+            for (i = 0; i < 10; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
