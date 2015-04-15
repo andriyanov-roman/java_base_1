474faf9b9d046e569024b396216f80e814e61077
Fijji <Neponatno@bigmir.net>
2015-04-15 18:52:11 +0300

    home work

---
+++ kirill/src/main/java/MassDead.java
@@ -0,0 +1,28 @@
+
+public class MassDead {
+    public static void main(String[] args) {
+        int[][] ms={
+            {1,2,4,5,10},
+            {4,6,22,12,4},
+            {0,13,22,38,42}
+        };
+
+      /*  for (int i = 0; i < ms.length; i++) {              //вывести
+            for (int j = 0; j < ms[i].length; j++) {
+                System.out.print(ms[i][j] + " ");
+            }
+            System.out.println();}
+
+        for(int o = 0; o < ms.length; o++){                  //вывести в обр сторону строки
+            for (int b = ms[o].length-1; b >= 0; b--) {
+                    System.out.print(ms[o][b] + " ");
+                }
+                System.out.println();}*/
+
+        for(int o =ms.length; o >= 0; o--){
+            for (int b = ms[o].length-1; b >= 0; b--) {
+                System.out.print(ms[o][b] + " ");
+            }
+            System.out.println();}
+    }
+}
