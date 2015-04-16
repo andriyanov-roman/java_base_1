208db8dd308eb2e08e5168bc68f3a633a787434c
randriyanov <randriyanov@mail.ru>
2015-04-16 20:55:16 +0300

    added examples

--- examples/src/java/str/StringsTests.java
+++ examples/src/java/str/StringsTests.java
@@ -5,6 +5,19 @@ package str;
  */
 public class StringsTests {
     public static void main(String[] args) {
+        String str1 = "Strings are immutable";
+        String str2 = "Strings are immutable";
+        String str3 = "Integers are not immutable";
+
+        int result = str1.compareTo( str2 );
+        System.out.println(result);
+
+        result = str2.compareTo( str3 );
+        System.out.println(result);
+
+        result = str3.compareTo( str1 );
+        System.out.println(result);
+
         int[] array = {};
         int a = 20000;
         char[] hello = {'h', 'e', 'l', 'l', 'o'};
