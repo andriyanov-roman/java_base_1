0452a23b2d6c5ba2dd1c2b9e983584695d00a11e
vortor <krotov1212@mail.ru>
2015-04-21 20:59:11 +0300

    test11

--- evgeniy/src/main/java/Std7v1.java
+++ evgeniy/src/main/java/Std7v1.java
@@ -9,14 +9,19 @@ import java.util.Scanner;
 public class Std7v1 {
     public static void main(String[] args) {
        Budy[] h=getBudys();
-        showBudys(h);
+        //showBudys(h);
         Scanner scanner = new Scanner(System.in);
+        System.out.println(h[0].run(2.00, 60));
 
     }
 
-    public static void showBudys(Budy[] Budys) {
-        for (int i = 0; i< Budy().length)
+    public static void showHumans(Budy[] Budys) {
+        for (int i = 0; i < Budys.length; i++) {
+            System.out.println(Budys[i].name + " " + Budys[i].secondName
+                    + " " + Budys[i].ads + " " + Budys[i].weight);
+        }
     }
+
 
 
     public static Budy[] getBudys() {
@@ -24,10 +29,10 @@ public class Std7v1 {
         Budy h2 = new Budy();
         Budy h3 = new Budy();
         Budy h4 = new Budy();
-        Budy h5 = new Budy();
-        Budy h6 = new Budy();
-        Budy h7 = new Budy();
-        Budy h8 = new Budy();
+       // Budy.budy h5 = new Budy.budy();
+       // Budy.budy h6 = new Budy.budy();
+      //  Budy.budy h7 = new Budy.budy();
+      //  Budy.budy h8 = new Budy.budy();
         h1.name = "Gaga";
         h1.secondName = "Gagagav";
         h1.ads = 22;
@@ -48,6 +53,6 @@ public class Std7v1 {
         h3.ads = 44;
         h3.height = 170;
         h3.weight = 155;
-        return new Budy[] {h1,h2,h3,h4};
+        return new Budy[] {h1,h2,h3};
     }
 }
