a6e2c18fc9faec51ee7288ef707c7d8c895e4053
dirutix <dirutix@list.ru>
2015-04-21 20:12:42 +0300

    Homework 07.04

---
+++ dmitriy/src/main/java/LessonTask/Human.java
@@ -0,0 +1,12 @@
+package LessonTask;
+
+/**
+ * Created by Дима on 21.04.2015.
+ */
+public class Human {
+    public String name;
+    public String surname;
+    public int age;
+    public double weight;
+    public double height;
+}
--- dmitriy/src/main/java/Main.java
+++ dmitriy/src/main/java/Main.java
@@ -2,4 +2,32 @@
  * Created by user on 3/31/2015.
  */
 public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int[] d = {1, 3, 7};
+        boolean[] booleans = {true, false, true, false};
+        int[] array = new int[5];
+        array[0] = 5;
+        array[1] = 15;
+        array[2] = 51;
+        array[3] = 115;
+        array[4] = 151;
+        System.out.println(array[0] + array[1] + array[2] + array[3] + array[4]);
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
+
+
+
+    }
 }
+
+
+
+
+
+
