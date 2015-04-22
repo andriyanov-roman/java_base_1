3ed946369292e8034d97bf85a3bf8eba6b2ef63a
Eugene <deyneko55@gmail.com>
2015-04-22 21:57:41 +0300

    HW7

--- evgen/src/main/java/Cakes/Humans.java
+++ evgen/src/main/java/Cakes/Humans.java
@@ -1,7 +1,7 @@
 package Cakes;
 
 /**
- * Created by Евгений on 21.04.2015.
+ * Created by пїЅпїЅпїЅпїЅпїЅпїЅпїЅ on 21.04.2015.
  */
 public class Humans {
     public String name;
@@ -11,20 +11,23 @@ public class Humans {
     public double height;
 
 
+
+
     public String walk() {
-        if (this.age < 5 && this.weight < 20 && this.height > 1.00) {
+        if (this.age > 0 && this.age < 6 && this.weight > 0 && this.weight < 21 && this.height > 0 && this.height < 1.01) {
             return "very slow";
-        } else if (this.age > 0 && this.age < 10 && this.weight < 40 && this.height < 1.20) {
+        } else if (this.age > 5 && this.age < 10 && this.weight > 20 && this.weight < 40 && this.height > 1.00 && this.height < 1.20) {
             return "not so fast";
-        } else if (this.age > 10 && this.age < 30 && this.weight < 80 && this.height < 1.85) {
+        } else if (this.age > 9 && this.age < 30 && this.weight > 39 && this.weight < 80 && this.height > 1.19 && this.height < 1.85) {
             return "very fast";
-        } else if (this.age > 30 && this.age < 50 && this.weight < 120 && this.height < 2.00) {
-            return "not so fast as you can";
-        } else if (this.age > 50 && this.age < 80 && this.weight < 120 && this.height < 2.00) {
+        } else if (this.age > 29 && this.age < 50 && this.weight > 79 && this.weight < 100 && this.height > 1.84 && this.height < 2.00) {
+            return "not so fast as he can";
+        } else if (this.age > 49 && this.age < 80 && this.weight > 99 && this.weight < 120 && this.height > 1.84 && this.height < 2.10) {
             return "slow";
-        } else if (this.age > 80 && this.age < 100 && this.weight < 120 && this.height < 2.00) {
-            return "very slow";
+        } else if (this.age > 79 && this.age < 100 && this.weight > 119 && this.weight < 200 && this.height > 1.84 && this.height < 2.10) {
+            return "very slow if he still alive";
         }
         return "unknown";
     }
+
 }
