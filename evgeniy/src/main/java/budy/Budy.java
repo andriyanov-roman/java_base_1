a22222891917db1e3a19c4488d0433b322aba3a5
vortor <krotov1212@mail.ru>
2015-04-23 20:59:34 +0300

    test11

---
+++ evgeniy/src/main/java/budy/Budy.java
@@ -0,0 +1,29 @@
+package budy;
+
+/**
+ * Created by Евгений on 23.04.15.
+ */
+public class Budy {
+    private String name;
+    private String secondName;
+    private int ads;
+    private double weight;
+    private double height;
+
+    public String run(double height, double weight) {
+        if (this.weight < weight && this.height > height) {
+            return "non";
+        } else if (this.weight > weight && this.height < height) {
+            return "blablabla";
+        }
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+    }
+}
