20a8c56b3be30dd9c70a7bae52ff0bf69c1b4381
Genasip <nightwatchftw@gmail.com>
2015-04-23 07:31:14 +0300

    KIBORGIIIII

--- gena/src/main/java/oop/Human.java
+++ gena/src/main/java/oop/Human.java
@@ -5,9 +5,59 @@ package oop;
  */
 public class Human {
     public int age;
-    public String name;
     public String secondName;
+    public String name;
     public double weight;
     public double height;
+
+
+    public void walking() {
+        if (age <= 2) {
+            System.out.print(" Ползает\n");
+        }
+        if (age > 2 && age <= 20) {
+            System.out.print(" Быстро ходит\n");
+        }
+        if (age > 20 && age <= 50) {
+            System.out.print(" Нормально ходит \n");
+        }
+        if (age > 50 && age <= 80) {
+            System.out.print(" Медленно ходит\n");
+        }
+        if (age > 80 && age <= 120) {
+            System.out.print(" Вероятно не ходи\n");
+        }
+        if (age > 120) {
+            System.out.print(" Лежит, но скоро пойдет \n");
+        }
+
+
+    }
+
+    public double getArgNum(String parametr){
+        double box = 0;
+        if (parametr.equals("age")){
+            box = age;
+        }
+        if (parametr.equals("height")){
+            box = height;
+        }
+        if (parametr.equals("weight")){
+            box = weight;
+        }
+        return box;
+    }
+    public String getArgString(String parametr){
+        String box = "";
+        if (parametr.equals("name")){
+            box = name;
+        }
+        if (parametr.equals("secondName")){
+            box = secondName;
+        }
+
+        return box;
+    }
+
 
 }
