3bd6c2e2101e478dadac0d218c460b5eff514bda
dirutix <dirutix@list.ru>
2015-04-27 15:37:32 +0300

    Homework 07.04

--- dmitriy/src/main/java/Homework_7/oop.java
+++ dmitriy/src/main/java/Homework_7/oop.java
@@ -7,151 +7,150 @@ public class oop {
     public static void main(String[] args) {
         Human[] person = getHumans();
         //поменять минимальное и максимальное значение местами
-        int min = person[0].surname.length();
-        int max = person[0].surname.length();
+        int min = person[0].getSurname().length();
+        int max = person[0].getSurname().length();
         int maxI = 0;
         int minI = 0;
         for (int i = 0; i < person.length; i++) {
-            if (person[i].surname.length() > max) {
-                max = person[i].surname.length();
+            if (person[i].getSurname().length() > max) {
+                max = person[i].getSurname().length();
                 maxI = i;
-            } else if (person[i].surname.length() < min) {
-                min = person[i].surname.length();
+            } else if (person[i].getSurname().length() < min) {
+                min = person[i].getSurname().length();
                 minI = i;
             }
         }
-        String temp = person[maxI].surname;
-        person[maxI].surname = person[minI].surname;
-        person[minI].surname = temp;
+        String temp = person[maxI].getSurname();
+        person[maxI].setSurname(person[min].getName());
+        person[minI].setSurname(temp);
         for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].surname);
+            System.out.println(person[i].getSurname());
         }
         //отсортировать по всем состояниям
         for (int i = 0; i < person.length; i++) {
             for (int j = 0; j < person.length - i - 1; ) {
-                if (person[j].name.length() > person[j + 1].name.length()) {
-                    String t = person[j + 1].name;
-                    person[j + 1].name = person[j].name;
-                    person[j].name = t;
+                if (person[j].getName().length() > person[j + 1].getName().length()) {
+                    String t = person[j + 1].getName();
+                    person[j + 1].setName(person[j].getName());
+                    person[j].setName(t);
                 }
             }
         }
         for (int i = 0; i < person.length; i++) {
             for (int j = 0; j < person.length - i - 1; ) {
-                if (person[j].surname.length() > person[j + 1].surname.length()) {
-                    String t = person[j + 1].surname;
-                    person[j + 1].surname = person[j].surname;
-                    person[j].surname = t;
+                if (person[j].getSurname().length() > person[j + 1].getSurname().length()) {
+                    String t = person[j + 1].getSurname();
+                    person[j + 1].setSurname(person[j].getSurname());
+                    person[j].setSurname(t);
                 }
             }
         }
         for (int i = 0; i < person.length; i++) {
             for (int j = 0; j < person.length - i - 1; ) {
-                if (person[j].age > person[j + 1].age) {
-                    int t = person[j + 1].age;
-                    person[j + 1].age = person[j].age;
-                    person[j].age = t;
+                if (person[j].getAge() > person[j + 1].getAge()) {
+                    int t = person[j + 1].getAge();
+                    person[j + 1].setAge(person[j].getAge());
+                    person[j].setAge(t);
                 }
             }
         }
         for (int i = 0; i < person.length; i++) {
             for (int j = 0; j < person.length - i - 1; ) {
-                if (person[j].height > person[j + 1].height) {
-                    double t = person[j + 1].height;
-                    person[j + 1].height = person[j].height;
-                    person[j].height = t;
+                if (person[j].getHeight() > person[j + 1].getHeight()) {
+                    double t = person[j + 1].getHeight();
+                    person[j + 1].setHeight(person[j].getHeight());
+                    person[j].setHeight(t);
                 }
             }
         }
         for (int i = 0; i < person.length; i++) {
             for (int j = 0; j < person.length - i - 1; ) {
-                if (person[j].weight > person[j + 1].weight) {
-                    double t = person[j + 1].weight;
-                    person[j + 1].weight = person[j].weight;
-                    person[j].weight = t;
+                if (person[j].getWeight() > person[j + 1].getWeight()) {
+                    double t = person[j + 1].getWeight();
+                    person[j + 1].setWeight(person[j].getWeight());
+                    person[j].setWeight(t);
                 }
             }
         }
         for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].name);
+            System.out.println(person[i].getName());
         }
         for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].surname);
+            System.out.println(person[i].getSurname());
         }
         for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].age);
+            System.out.println(person[i].getAge());
         }
         for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].height);
+            System.out.println(person[i].getHeight());
         }
         for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].weight);
+            System.out.println(person[i].getWeight());
         }
-
 
     }
 
     public static Human[] getHumans() {
         Human h1 = new Human();
-        h1.name = "Dima";
-        h1.surname = "Andry";
-        h1.age = 16;
-        h1.height = 1.74;
-        h1.weight = 65.0;
+        h1.setName("Dima");
+        h1.setSurname("Andry");
+        h1.setAge(16);
+        h1.setHeight(1.74);
+        h1.setWeight(65.0);
         Human h2 = new Human();
-        h2.name = "Dima";
-        h2.surname = "Sid";
-        h2.age = 16;
-        h2.height = 1.65;
-        h2.weight = 50.0;
+        h2.setName("Dima");
+        h2.setSurname("Sid");
+        h2.setAge(16);
+        h2.setHeight(1.65);
+        h2.setWeight(50.0);
         Human h3 = new Human();
-        h3.name = "Vova";
-        h3.surname = "Logvinenko";
-        h3.age = 15;
-        h3.height = 1.62;
-        h3.weight = 55.0;
+        h3.setName("Vova");
+        h3.setSurname("Logvinenko");
+        h3.setAge(15);
+        h3.setHeight(1.62);
+        h3.setWeight(55.0);
         Human h4 = new Human();
-        h4.name = "Danil";
-        h4.surname = "Pisarenko";
-        h4.age = 17;
-        h4.height = 1.80;
-        h4.weight = 75.0;
+        h4.setName("Danil");
+        h4.setSurname("Pisarenko");
+        h4.setAge(17);
+        h4.setHeight(1.80);
+        h4.setWeight(75.0);
         Human h5 = new Human();
-        h5.name = "Pavel";
-        h5.surname = "Karpenko";
-        h5.age = 16;
-        h5.height = 1.79;
-        h5.weight = 80.0;
+        h5.setName("Pavel");
+        h5.setSurname("Karpenko");
+        h5.setAge(16);
+        h5.setHeight(1.79);
+        h5.setWeight(80.0);
         Human h6 = new Human();
-        h6.name = "Valera";
-        h6.surname = "Bass";
-        h6.age = 16;
-        h6.height = 1.76;
-        h6.weight = 65.0;
+        h6.setName("Valera");
+        h6.setSurname("Bass");
+        h6.setAge(16);
+        h6.setHeight(1.76);
+        h6.setWeight(65.0);
         Human h7 = new Human();
-        h7.name = "Marina";
-        h7.surname = "Tykhonenko";
-        h7.age = 15;
-        h7.height = 1.69;
-        h7.weight = 40.0;
+        h7.setName("Marina");
+        h7.setSurname("Tykhonenko");
+        h7.setAge(15);
+        h7.setHeight(1.69);
+        h7.setWeight(40.0);
         Human h8 = new Human();
-        h8.name = "Nastya";
-        h8.surname = "Artemenko";
-        h8.age = 15;
-        h8.height = 1.58;
-        h8.weight = 40.0;
+        h8.setName("Nastya");
+        h8.setSurname("Artemenko");
+        h8.setAge(15);
+        h8.setHeight(1.58);
+        h8.setWeight(40.0);
         Human h9 = new Human();
-        h9.name = "Katya";
-        h9.surname = "Kiblitskaya";
-        h9.age = 15;
-        h9.height = 1.74;
-        h9.weight = 40.0;
+        h9.setName("Katya");
+        h9.setSurname("Kiblitskaya");
+        h9.setAge(15);
+        h9.setHeight(1.74);
+        h9.setWeight(40.0);
         Human h10 = new Human();
-        h10.name = "Liza";
-        h10.surname = "Gansevich";
-        h10.age = 16;
-        h10.height = 1.71;
-        h10.weight = 50;
+        h10.setName("Liza");
+        h10.setSurname("Gansevich");
+        h10.setAge(16);
+        h10.setHeight(1.71);
+        h10.setWeight(50);
         return new Human[]{h1, h2, h3, h4, h5, h6, h7, h8, h9, h10};
 
     }
