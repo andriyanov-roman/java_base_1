0b5ba87b7b2bf4396a6536e10f8233a33ed5a255
Dima <dirutix@list.ru>
2015-04-30 17:02:15 +0300

    Some changes

---
+++ dmitriy/src/main/java/Homework_7_oop_tasks/Admin.java
@@ -0,0 +1,52 @@
+package Homework_7_oop_tasks;
+
+/**
+ * Created by Дима on 30.04.2015.
+ */
+public class Admin {
+    private int age;
+    private int scale;
+    private String name;
+    private String surname;
+    private String platform;
+
+    public int getScale() {
+        return scale;
+    }
+
+    public void setScale(int scale) {
+        this.scale = scale;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
