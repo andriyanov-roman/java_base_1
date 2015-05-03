f581905a71743784674b97ac10274c3e65badc79
vortor <krotov1212@mail.ru>
2015-05-03 22:36:42 +0300

    test11

--- evgeniy/src/main/java/HTask/companyAvto/Avto.java
+++ evgeniy/src/main/java/HTask/companyAvto/Avto.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class Avto {
     private String nameAvto;
     private String typeAvto;
-    private String holder; //owner
+    private String holder;
     private String numberAvto;
     private String colorAvto;
     private String factoryName;
