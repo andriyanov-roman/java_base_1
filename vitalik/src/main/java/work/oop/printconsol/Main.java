4c4156a043b2bc83a6c60781cc6854f0807876cf
VitalijGorbunov <Vitalij.Gorbunov@yahoo.com>
2015-05-12 20:46:25 +0300

    Home work

--- vitalik/src/main/java/work/oop/printconsol/Main.java
+++ vitalik/src/main/java/work/oop/printconsol/Main.java
@@ -3,5 +3,8 @@ package work.oop.printconsol;
 /**
  * Created by Виталий on 12.05.15.
  */
-public class ReadFile {
+public class Main {
+    public static void main(String[] args) {
+        
+    }
 }
