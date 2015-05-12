aeb35f34de59a59959c10d0382a7f00836d72f07
Fijji <Neponatno@bigmir.net>
2015-05-12 18:05:45 +0300

    home work

---
+++ kirill/src/home/hwNEW/OnlyOne/Test.java
@@ -0,0 +1,27 @@
+package OnlyOne;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+/**
+ * Created by Pipin on 09.05.2015.
+ */
+public class Test {
+    public static void main(String[]args){
+
+        One n1 = new One();
+        n1.setName("boris");
+        n1.setAge(10);
+        n1.setWeight(50);
+
+        System.out.println(n1.getName()+" "+n1.getAge()+" "+n1.getWeight());
+
+        One n2 = new One();
+        n2.setName("Vova");
+        n2.setAge(90);
+        n2.setWeight(40);
+
+        System.out.println(n2.getName() + " " + n2.getAge() + " " + n2.getWeight());
+
+
+}
+}
