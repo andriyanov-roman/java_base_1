ef91d635fbfc4206d67afe7ec845603370251c78
dirutix <dirutix@list.ru>
2015-05-12 20:52:35 +0300

    Homework 07.04

---
+++ dmitriy/src/main/java/uncasual/ReadToConsole.java
@@ -0,0 +1,10 @@
+package uncasual;
+
+/**
+ * Created by Дима on 12.05.2015.
+ */
+public interface ReadToConsole {
+    void readBySquare();
+    void readByNKH();
+
+}
---
+++ dmitriy/src/main/java/uncasual/UnusualMethods.java
@@ -0,0 +1,19 @@
+package uncasual;
+
+import java.io.File;
+
+/**
+ * Created by Дима on 12.05.2015.
+ */
+public class UnusualMethods implements ReadToConsole {
+
+    public void readBySquare() {
+        File f = new File("dmitriy//unusual.txt");
+
+
+    }
+
+    public void readByNKH() {
+
+    }
+}
