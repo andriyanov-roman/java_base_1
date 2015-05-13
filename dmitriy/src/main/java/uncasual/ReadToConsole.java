82f29b23ac8cf2200bb49f7f3bd7b8a51dd875e3
dirutix <dirutix@list.ru>
2015-05-13 18:19:05 +0300

    Homework 07.04

--- dmitriy/src/main/java/uncasual/ReadToConsole.java
+++ dmitriy/src/main/java/uncasual/ReadToConsole.java
@@ -1,10 +1,13 @@
 package uncasual;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Created by Дима on 12.05.2015.
  */
 public interface ReadToConsole {
-    void readBySquare();
+    void readBySquare() throws IOException;
     void readByNKH();
 
 }
--- dmitriy/src/main/java/uncasual/UnusualMethods.java
+++ dmitriy/src/main/java/uncasual/UnusualMethods.java
@@ -1,14 +1,23 @@
 package uncasual;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by Дима on 12.05.2015.
  */
 public class UnusualMethods implements ReadToConsole {
 
-    public void readBySquare() {
+    public void readBySquare() throws IOException {
         File f = new File("dmitriy//unusual.txt");
+        FileReader reader = new FileReader(f);
+        BufferedReader buffer = new BufferedReader(reader);
+        String line;
+        while (buffer.readLine() != null){
+            String[] pool = buffer.readLine().split(":");
+
+
+        }
+
 
 
     }
