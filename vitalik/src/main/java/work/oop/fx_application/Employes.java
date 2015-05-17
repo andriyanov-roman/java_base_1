9060ddbf373278381dc1a00d044775204eab7a6b
VitalijGorbunov <Vitalij.Gorbunov@yahoo.com>
2015-05-17 17:50:25 +0300

    fx

---
+++ vitalik/src/main/java/work/oop/fx_application/Employes.java
@@ -0,0 +1,43 @@
+package work.oop.fx_application;
+
+/**
+ * Created by Александра on 17.05.2015.
+ */
+public class Employes {
+    private String name;
+    private String secondName;
+    private int age;
+    private int salary;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
---
+++ vitalik/src/main/java/work/oop/fx_application/FxForm.java
@@ -0,0 +1,150 @@
+package work.oop.fx_application;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class FxForm extends Application {
+    private File file = new File("vitalik\\resources\\Fx.txt");
+    private ArrayList<Employes> employes = new ArrayList<Employes>();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("FX Application");
+        GridPane grid = getPane();
+        Scene scene = new Scene(grid, 330, 400);
+        setComponets(grid);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+    private void setComponets (GridPane grid) throws IOException{
+        Label labelAdd = new Label("Add new Employee:");
+        labelAdd.setFont(Font.font("Tahoma", FontWeight.BOLD, 12));
+        grid.add(labelAdd, 0,  0);
+        Label labelSave = new Label("Save  Employee:");
+        labelSave.setFont(Font.font("Tahoma", FontWeight.BOLD, 12));
+        grid.add(labelSave, 0, 7);
+
+        Label labelName = new Label("Name:");
+        grid.add(labelName, 0, 1);
+        final TextField name = new TextField();
+        grid.add(name, 1, 1);
+
+        Label labelSecondName = new Label("Second Name:");
+        grid.add(labelSecondName, 0, 2);
+        final TextField secondName = new TextField();
+        grid.add(secondName, 1, 2);
+
+        Label salaryLabel = new Label("Salary:");
+        grid.add(salaryLabel, 0, 3);
+        final TextField salary = new TextField();
+        grid.add(salary, 1, 3);
+
+        Label ageLabel = new Label("Age:");
+        grid.add(ageLabel, 0, 4);
+        TextField age = new TextField();
+        grid.add(age, 1, 4);
+
+
+
+        ToggleGroup group = new ToggleGroup();
+
+        RadioButton rb1 = new RadioButton("no sorting");
+        rb1.setToggleGroup(group);
+        rb1.setSelected(true);
+        grid.add(rb1, 0, 8);
+
+        RadioButton rb2 = new RadioButton("sorting by salary");
+        rb2.setToggleGroup(group);
+        grid.add(rb2, 0, 9);
+
+        RadioButton rb3 = new RadioButton("sort by age");
+        rb3.setToggleGroup(group);
+        grid.add(rb3, 0, 10);
+
+        Button button = new Button("  Add  ");
+        Button button1 = new Button("  Save   ");
+
+
+        final FileWriter writer = new FileWriter(file, true);
+        final Label info = new Label();
+        info.setFont(Font.font("Tahoma", FontWeight.BLACK, 10));
+        grid.add(info, 1, 5);
+//        button.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent e) {
+//                try {
+//                    if(!name.getText().isEmpty()
+//                            && !salary.getText().isEmpty()
+//                            && !secondName.getText().isEmpty()) {
+//                        Employee employee = new Employee();
+//                        employee.setName(name.getText());
+//                        employee.setSalary(Double.parseDouble(salary.getText()));
+//                        employee.setSecondName(secondName.getText());
+//                        employees.add(employee);
+//                        info.setText("Employee Added");
+//                    } else {
+//                        info.setText("Employee not Added");
+//                    }
+//                    writerToFile(writer, name.getText(), secondName.getText(), salary.getText());
+//                } catch (Exception e1) {
+//                    e1.printStackTrace();
+//                }
+//            }
+//        });
+
+
+
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.CENTER_LEFT);
+        hbBtn.getChildren().add(button);
+        grid.add(button, 0, 5);
+        hbBtn.getChildren().add(button1);
+        button1.setAlignment(Pos.CENTER_LEFT);
+        grid.add(button1, 0, 11);
+
+
+    }
+    private void writerToFile(FileWriter writer, String... args) throws IOException {
+        for (int i = 0; i < args.length; i++) {
+            writer.write(args[i] + " ");
+        }
+        writer.write('\n');
+        writer.flush();
+        writer.close();
+    }
+    private GridPane getPane() {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_LEFT);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        return grid;
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
