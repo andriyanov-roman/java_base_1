3d794aa543c26305409a678d1809af9ede128362
Fijji <Neponatno@bigmir.net>
2015-05-19 19:06:06 +0300

    home work

---
+++ kirill/src/home/hwNEW/jfx/Employee.java
@@ -0,0 +1,38 @@
+package jfx;
+
+public class Employee implements Comparable<Employee> {
+    private String name;
+    private String secondName;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        int compareSalary = o.getSecondName().length();
+        return compareSalary - this.secondName.length();
+    }
+}
+
--- kirill/src/main/java/cla.java
+++ kirill/src/main/java/cla.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class cla {
     public static void main(String[] args) throws IOException {
 
-new Jfx().startProgram(args);
+new Jfx().start(args);
        /*ArrayList gogo = new ArrayList();
         gogo.add(Office.getAdmins());
 
